d4be533960c1e1ffd685430b463779f424cf9e8e
Venkata R. Muliki <Venkata.Muliki@MAXIMAPUNE.COM>
2024-07-21 00:22:02 +0530

    first commit

---
+++ src/main/java/com/mango/jelly/MangoJellyApplication.java
@@ -0,0 +1,13 @@
+package com.mango.jelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoJellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoJellyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mango/jelly/MangoJellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.jelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoJellyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
